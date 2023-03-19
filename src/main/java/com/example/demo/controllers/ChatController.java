fddd280c7e2b9c3dd004d01e958b45b694244cd1
Cfnatic7 <78828448+Cfnatic7@users.noreply.github.com>
2023-03-20 00:09:13 +0100

    fix: sending message to private user

--- src/main/java/com/example/demo/controllers/ChatController.java
+++ src/main/java/com/example/demo/controllers/ChatController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -24,15 +25,14 @@ public class ChatController {
     }
 
     @MessageMapping("/sendMessageToRoom")
-    public void sendMessageToRoom(ChatMessage chatMessage) {
+    public void sendMessageToRoom(@Payload ChatMessage chatMessage) {
         messagingTemplate.convertAndSend("/topic/rooms/" + chatMessage.getRoomId(),
                 chatMessage.getMessage());
     }
 
     @MessageMapping("/sendPrivateMessage")
-    public void sendPrivateMessage(PrivateMessage privateMessage) {
-        messagingTemplate.convertAndSendToUser(privateMessage.getReceiver(),
-                "/private",
+    public void sendPrivateMessage(@Payload PrivateMessage privateMessage) {
+        messagingTemplate.convertAndSend("/queue/user/" + privateMessage.getReceiver(),
                 privateMessage.getMessage());
     }
 
